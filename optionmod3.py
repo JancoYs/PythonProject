aca88ddc7b07f0b46bb7e572082676e1610fe620
JancoYs <jancoyspeert@gmail.com>
2014-04-29 01:29:39 +0200

    Update optionmod3.py

--- optionmod3.py
+++ optionmod3.py
@@ -1,41 +1,38 @@
 #! /usr/bin/python
-import sys
-import os
 
 def AlignmentExplorer(sexTup, alignS, filename):
   
   print
   start = input('Enter the start of segment: ')
   print
-  end = input('Enter the start of segment: ')
-  print "\n\n\n"
+  end = input('Enter the end of the segment: ')
+  print "\n\n"
   print "Clustal segment [",start,"-",end,"] of the ",filename," alignment."
   print "\n"
   
-
+  
   if (end-start)<61:
     for i in range(0,len(sexTup)):
       print sexTup[i][0], "\t", sexTup[i][1][start:end]
-      print '\t', alignS[start:end] 
-  raw_input('Press E to enter another section or Enter to return to menu: ')
-  if str(input) == "E":
-    AlignmentExplorer(sexTup, alignS, filename)
-  else:
-    os.system('clear')
-    return 0 
+    print '\t', alignS[start:end] 
+    E = raw_input('Press E to explore another sequence, or Enter to return to menu: ')
+    if str(E)=="E":
+      return 1
+    else:
+      return 0
   
-  if (end-start)>60:
-    while (end-start)>60:
-      for i in range(0,len(sexTup)):
-	print sexTup[i][0], "\t", sexTup[i][1][start:+60]
-	print '\t', alignS[start:start+60]
-	print "\n"
-	start = start+60
-  print sexTup[i][0], "\t", sexTup[i][1][start:end]
+  while (end-start)>60:
+    endfornow = start+60
+    for i in range(0,len(sexTup)):
+      print sexTup[i][0], "\t", sexTup[i][1][start:endfornow]
+    print '\t', alignS[start:endfornow]
+    print "\n"
+    start = start+60
+  for i in range(0,len(sexTup)):  
+    print sexTup[i][0], "\t", sexTup[i][1][start:end]
   print '\t', alignS[start:end]
-  raw_input('Press E to enter another section or Enter to return to menu: ')
-  if str(input) == "E":
-    AlignmentExplorer(sexTup, alignS, filename)
+  E = raw_input('Press E to explore another sequence, or Enter to return to menu: ')
+  if str(E)=="E":
+    return 1
   else:
-    os.system('clear')
-    return 0 
+    return 0
